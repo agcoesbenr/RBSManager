c4b055018726bc257ef41bfe455e9c64f6da262b
Esben Bücking-Rasmussen <esben.bueckingrasmussen@agcocorp.com>
2022-01-11 11:11:08 +0100

    Changes to message layout class

--- RBSManager/Classes/Messages/Standard/Float64MultiArrayMessage.swift
+++ RBSManager/Classes/Messages/Standard/Float64MultiArrayMessage.swift
@@ -8,12 +8,22 @@ import UIKit
 import ObjectMapper
 
 public class Float64MultiArrayMessage: RBSMessage {
-	public var layout: MultiArrayLayout
-	public var data: float64[] = [float64[]]()
+	public var layout: MultiArrayLayoutMessage?
+    public var data: [Float64]?
+    
+    public override init() {
+        super.init()
+        layout = MultiArrayLayoutMessage()
+    }
+    
+    public required init?(map: Map) {
+        super.init(map: map)
+    }
     
 
     public override func mapping(map: Map) {
 	    layout <- map["layout"]
-		data <- map["data"]
+		data <- (map["data"])
     }
 }
+
